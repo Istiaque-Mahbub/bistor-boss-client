4ded5154dece4c5111378128cd0c3af7f3782b0a
Istiaque Mahbub <istiaquemahbub1@gmail.com>
2025-01-03 09:44:51 +0600

    use custom use and fetch data by axiosSecure

--- src/components/foodCart/FoodCart.jsx
+++ src/components/foodCart/FoodCart.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+
 
 const FoodCart = ({item}) => {
-    const {name,image,recipe,price} = item
+    const {name,image,recipe,price,_id} = item;
+    const {user} = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const axiosSecure = useAxiosSecure();
+    const handleAddToCart = food =>{
+      if(user && user.email){
+        //sent cart item to the database
+        const cartItem = {
+          menuId: _id,
+          email:user.email,
+          name,
+          image,
+          price
+        }
+        axiosSecure.post('/carts',cartItem)
+        .then(res=>{
+          console.log(res.data)
+          if(res.data.insertedId){
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: `${name} added to your cart`,
+              showConfirmButton: false,
+              timer: 1500
+            });
+          }
+        })
+      }
+      else{
+        Swal.fire({
+          title: "You are not login",
+          text: "Please login to add to the cart",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, login!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            //send the user to the login page
+            navigate('/login',{state:{from: location}})
+          }
+        });
+      }
+    }
     return (
         <div className="card bg-base-100 w-96 shadow-xl">
   <figure>
@@ -15,7 +64,8 @@ const FoodCart = ({item}) => {
     <h2 className="card-title">{name}</h2>
     <p>{recipe}</p>
     <div className="card-actions justify-center">
-      <button className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4">Add Cart</button>
+      <button onClick={()=>handleAddToCart(item)}
+      className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4">Add to Cart</button>
     </div>
   </div>
 </div>
--- src/pages/shared/navBar/NavBar.jsx
+++ src/pages/shared/navBar/NavBar.jsx
@@ -1,66 +1,74 @@
 import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { AuthContext } from '../../../providers/AuthProvider';
-
+import { FaCartArrowDown } from "react-icons/fa";
 const NavBar = () => {
-  const {user,logOut} = useContext(AuthContext)
-  const handleLogout = () =>{
+  const { user, logOut } = useContext(AuthContext)
+  const handleLogout = () => {
     logOut()
-    .then(()=>{})
-    .catch(err=>console.log(err.message))
+      .then(() => { })
+      .catch(err => console.log(err.message))
   }
-    const links = <>
+  const links = <>
     <li><NavLink to='/'>Home</NavLink></li>
     <li><NavLink to='/menu'>Our Menu</NavLink></li>
     <li><NavLink to='/order/salads'>Order Food</NavLink></li>
     <li><NavLink to='/secret'>Secret</NavLink></li>
-    
-      {
-        user ? <>
-        <span>{user?.displayName}</span>
-        <li  className='font-bold' onClick={handleLogout}><NavLink>Logout</NavLink></li>
-        
-        </> :<>
+    <li>
+      <Link to='/'>
+        <button className="btn btn-sm">
+          <FaCartArrowDown></FaCartArrowDown>
+          <div className="badge text-sm badge-secondary">+0</div>
+        </button>
+      </Link>
+    </li>
+
+    {
+      user ? <>
+        {/* <span>{user?.displayName}</span> */}
+        <li className='font-bold' onClick={handleLogout}><NavLink>Logout</NavLink></li>
+
+      </> : <>
         <li><NavLink to='/login'>Login</NavLink></li>
-        </>
-      }
-    </>
-    return (
-        <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h8m-8 6h16" />
-        </svg>
+      </>
+    }
+  </>
+  return (
+    <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white max-w-screen-xl">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16" />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+            {links}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Bistro Boss</a>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        {links}
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          {links}
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
     </div>
-    <a className="btn btn-ghost text-xl">Bistro Boss</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-     {links}
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-    );
+  );
 };
 
 export default NavBar;
